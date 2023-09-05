f7ab97e440aadc56d2b391364446c49bbe226dca
Shivam Kansagara <202001086@daiict.ac.in>
2023-09-05 10:54:21 +0530

    changes in todo list

--- ToDo_List/app.js
+++ ToDo_List/app.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app = express();
 let items = [];
+let workitems = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-  res.render("lists", { kindofday: day, newitem: items });
+  let day = date.getdate();
+  res.render("lists", { listitem: day, newitem: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newitem;
-  items.push(item);
-  res.redirect("/");
+  console.log(req.body);
+  if (req.body.button != "work") {
+    items.push(item);
+    res.redirect("/");
+  } else {
+    workitems.push(item);
+    res.redirect("/work");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("lists", { listitem: "work list", newitem: workitems });
 });
 
 app.listen(3000, function () {
-  console.log("Server in running on port 3000");
+  console.log("Server is running on port 3000");
 });
